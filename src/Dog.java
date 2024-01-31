fb22d8a40d34940782214ba54401777d2ac21bc3
Kirsten Mork <klmork@calpoly.edu>
2024-01-31 10:23:53 -0800

    Update Base to Match Handout

--- src/Dog.java
+++ src/Dog.java
@@ -1,5 +1,9 @@
+import java.util.Objects;
+
 public class Dog implements Pet {
+
     public void speak() {
         System.out.println("bark");
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Pet p = new Dog();
-        Dog d = new Dog();
-        p.speak();
-        d.speak();
 
+        Dog d1 = new Dog();
+        d1.speak();
+
+        Dog d2 = new Dog();
+
+        List<Dog> dogs = new ArrayList<>(Arrays.asList(d1, d2));
+        for (Dog dog : dogs) {
+            dog.speak();
+        }
+
+        Cat c = new Cat();
+        c.speak();
+
+        // TODO: Loop over a list of cats AND dogs and call speak?
+        // What would you have to do without the interface?
     }
 }
